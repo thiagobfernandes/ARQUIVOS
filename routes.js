35f0332662e7cc5efc8320e88d38fbecd60e5d8d
thiagobfernandes <thiago5437br@gmail.com>
2024-08-05 16:10:06 -0300

    thiagolindo

--- routes.js
+++ routes.js
@@ -11,22 +11,35 @@ const __dirname = dirname(fileURLToPath(import.meta.url)); // importando e confi
 const router= Router();
 
 router.get('/', async (req,res) => {
-try{
-    const dados = await receberip();
-    const pastaDestino = path.join(__dirname, 'ips')
-    if(!fs.existsSync(pastaDestino)) {
-      fs.mkdirSync(pastaDestino, {recursive:true})
+  const pastaDestino = path.join(__dirname, 'ips')
 
-    }
-    const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
-    const filePath = path.join(pastaDestino, `ip_info_${timestamp}.json`); // na parte do replae estou falando, olha quando for criar a string na coloque dois pontos substitua por -
-    fs.writeFileSync(filePath, JSON.stringify(dados, null, 2), 'utf-8');
-    res.status(200).json("")
-} catch(err){
-    res.status(500).json({
-      error:"internal server error"
-    });
+  try {
+    fs.accessSync(pastaDestino, fs.constants.W_OK);
+    console.log('Permissão de escrita verificada');
+} catch (err) {
+    console.error('Sem permissão de escrita para a pasta:', pastaDestino, err);
+    res.status(500).json({ error: "Sem permissão de escrita" });
+    return;
 }
+
+
+
+try{
+      const dados = await receberip();
+      
+      if(!fs.existsSync(pastaDestino)) {
+        fs.mkdirSync(pastaDestino, {recursive:true})
+
+      }
+      const timestamp = new Date().toISOString().replace(/[:.]/g, '-');
+      const filePath = path.join(pastaDestino, `ip_info_${timestamp}.json`); // na parte do replae estou falando, olha quando for criar a string na coloque dois pontos substitua por -
+      fs.writeFileSync(filePath, JSON.stringify(dados, null, 2), 'utf-8');
+      res.status(200).json("")
+  } catch(err){
+      res.status(500).json({
+        error:"internal server error"
+      });
+  }
 
 });
 
